2e628e9cdf89d554879c12ab39f1135ff3898108
stormont <stormont@gmail.com>
2014-11-05 23:32:16 -0800

    Added Part 2 of the continuous build system

--- Main.hs
+++ Main.hs
@@ -1,48 +1,80 @@
 
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
 import System.Directory
 import System.Environment (getArgs)
 import System.INotify
 import System.IO
 
 
+data Config =
+  Config
+    { confCabalFile :: FilePath
+    , confWorking :: Bool
+    } deriving (Show,Read)
+
+
 main = do
   args <- getArgs
   let dir = head args
   putStrLn $ "Watching directory: " ++ dir
+  contents <- getDirectoryContents dir
+  let contents' = filter filterCabal contents
+  case contents' of
+    (x:_) -> runThread x dir
+    [] -> do
+      putStrLn "No cabal file found!"
+      putStrLn "Exiting"
+
+
+runThread cabal dir = do
+  config <- newTVarIO $ Config cabal False
   n <- initINotify
   putStrLn "Press <Enter> to exit"
   print n
   wd <- addWatch n
                  [ Modify, CloseWrite, Create, Delete, MoveIn, MoveOut ]
                  dir
-                 eventHandler
+                 (eventHandler config)
   print wd
   getLine
   removeWatch wd
   killINotify n
 
 
-eventHandler :: Event -> IO ()
-eventHandler x@(Modified _ (Just fp)) = handleFilteredFile x fp
-eventHandler x@(MovedIn _ fp _) = handleFilteredFile x fp
-eventHandler x@(MovedOut _ fp _) = handleFilteredFile x fp
-eventHandler x@(Created _ fp) = handleFilteredFile x fp
-eventHandler x@(Deleted _ fp) = handleFilteredFile x fp
-eventHandler _ = return ()
+eventHandler :: TVar Config -> Event -> IO ()
+eventHandler conf x@(Modified _ (Just fp)) = handleFilteredFile conf x fp
+eventHandler conf x@(MovedIn _ fp _) = handleFilteredFile conf x fp
+eventHandler conf x@(MovedOut _ fp _) = handleFilteredFile conf x fp
+eventHandler conf x@(Created _ fp) = handleFilteredFile conf x fp
+eventHandler conf x@(Deleted _ fp) = handleFilteredFile conf x fp
+eventHandler _ _ = return ()
 
 
-handleFilteredFile evt fp = do
+handleFilteredFile conf evt fp = do
   if filterHS fp
-    then print evt >> doWork fp
+    then print evt >> doWork conf fp
     else return ()
 
 
-filterHS fp = (== "hs")
-            $ reverse
-            $ takeWhile (/= '.')
-            $ reverse fp
+filterHS fp = fileExt fp == "hs"
+filterCabal fp = fileExt fp == "cabal"
 
 
-doWork :: FilePath -> IO ()
-doWork fp = return ()
+fileExt = reverse
+        . takeWhile (/= '.')
+        . reverse
+
+
+doWork :: TVar Config -> FilePath -> IO ()
+doWork conf fp = do
+  config <- readTVarIO conf
+  if confWorking config
+    then do
+      print "Already working!"
+      return ()
+    else do
+      print "New work available!"
+      atomically $ writeTVar conf (config { confWorking = True })
+      return ()
 
